d3ecbc3ff5912438ac672baed47b945a02e128dc
Binh Lam <cindybinh444@gmail.com>
2018-09-03 14:47:22 -0400

    add pseudo-code

---
+++ assets/javascript/app.js
@@ -0,0 +1,19 @@
+
+// Add your own API key between the ""
+//var APIKey = "qJpvZJ0Hc1vfwfvut2JeN4VkhNWUPGQX";
+//var searchTerm = $("#form-value").val().trim();
+ // URL to query the database
+//var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + searchTerm +"&limit=10&rating="+"&api_key=" + APIKey;
+
+
+//There will be some buttons on the page around 4. 
+//Once the page is done loading 
+//The user is able to see the 4 buttons 
+//once the user clicks on the button, 10 gifs will display
+//when you click on the gif, it will move and 
+//when you click again, it will stop moving 
+
+//And the submit button is clicked 
+//a new button is added to the gif-wordlist after the forth button 
+//when you click on the new button, it will display 10 gifs in regards to the button
+
